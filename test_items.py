dca0e9b1f8668f2e6a1bd430dfe6bcb9705392b8
megabitik <megabitik@gmail.com>
2021-04-13 16:14:53 +0700

    Fixed sleep function location - it was after assert

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from time import sleep
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_add_to_basket_button_must_present(browser):
     browser.get(link)
     sleep(30)
     assert browser.find_element_by_css_selector("button.btn-add-to-basket"), f'Button "Add to basket" is not found'
